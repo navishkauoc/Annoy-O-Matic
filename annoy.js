5ec2187aab3cd5697348be6c962f07b194267b08
Navishka Kularathna <navishka.randika@gmail.com>
2019-10-04 16:37:32 +0530

    Modified

--- annoy.js
+++ annoy.js
@@ -1,6 +1,6 @@
 var answer = prompt("Are we there yet?");
 
-while(answer !== "yes" && answer !== "yeah"){
+while(answer.indexOf("yes") == -1 && answer.indexOf("yeah") == -1){
 	var answer = prompt("Are we there yet?");
 }
 
